41e2cf9e8756c52eb2f5d5e14649856cd5dd7f11
JethroNatividad <natividad.jet@gmail.com>
2024-01-30 06:49:48 +0800

    implement get random answer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::seq::SliceRandom;
+
 // A program that asks for a question, and outputs one of these randomly “Yes,” “No,” “Maybe,” or “Ask again later.”
 // Inputs: Question
 // Process: Select from "Yes", "No", "Maybe", "Ask again later." randomly
@@ -5,7 +7,9 @@
 
 fn get_random_answer(answers: Vector<&str>) -> &str {
     // Choose using rand
+    let answer: &str = answers.choose(&mut rand::thread_rng());
     // return answer
+    answer
 }
 #[cfg(test)]
 mod tests {
